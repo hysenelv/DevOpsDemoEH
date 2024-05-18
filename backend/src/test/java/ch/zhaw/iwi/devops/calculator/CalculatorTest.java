9c942cf2d984ba56c77710f72daacc668f652a7f
Elvis Hyseni <hysenelv@students.zhaw.ch>
2024-05-18 02:30:07 +0200

    subtract

--- backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTest.java
@@ -3,6 +3,8 @@ package ch.zhaw.iwi.devops.calculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import main.java.ch.zhaw.iwi.devops.calculator.Calculator;
+
 public class CalculatorTest {
 
 
@@ -12,6 +14,10 @@ public class CalculatorTest {
         Assertions.assertEquals(6, Calculator.calculate(3, 3, "add"));
     }
 
+  @Test
+    public void testCalculateSubtract() {
+        Assertions.assertEquals(3, Calculator.calculate(6, 3, "subtract"));
+    }
   
 }
 
